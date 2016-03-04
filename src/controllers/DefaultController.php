00aab5588b390c7618978629cfa675eef1b546e5
Tobias Munk <schmunk@usrbin.de>
2016-03-04 19:48:18 +0100

    added access control via trait

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -2,10 +2,13 @@
 
 namespace dmstr\modules\prototype\controllers;
 
+use dmstr\web\traits\AccessBehaviorTrait;
 use yii\web\Controller;
 
 class DefaultController extends Controller
 {
+    use AccessBehaviorTrait;
+
     public function actionIndex()
     {
         return $this->render('index');
