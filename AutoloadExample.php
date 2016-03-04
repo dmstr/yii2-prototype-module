1a96bb304fa95799c80e9d55cbc732003d03caf1
Tobias Munk <schmunk@usrbin.de>
2016-03-04 18:59:16 +0100

    initial commit

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace dmstr\modules\prototype;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
