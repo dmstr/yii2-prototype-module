6dd94efb061d0401ffad57e1a727ebbf99ffebd2
Tobias Munk <schmunk@usrbin.de>
2016-03-04 19:02:09 +0100

    added generated CRUDs

---
+++ src/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace dmstr\modules\prototype;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'dmstr\modules\prototype\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
