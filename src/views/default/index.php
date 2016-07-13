4ee5aee44f2c07feb5002fa413a54381e8e0c314
Tobias Munk <schmunk@usrbin.de>
2016-07-13 02:47:13 +0200

    updated module index view

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -1,38 +1,62 @@
 <?php
+
+namespace _;
+
+use insolita\wgadminlte\SmallBox;
 use rmrevin\yii\fontawesome\FA;
+use yii\helpers\Html;
 
 /*
  * @var yii\web\View $this
  */
-$controllers = \dmstr\helpers\Metadata::getModuleControllers($this->context->module->id);
-$favourites = [];
-
-$patterns = [
-    '^default$' => ['color' => 'gray', 'icon' => FA::_CUBE],
-    '^.*$' => ['color' => 'blue', 'icon' => FA::_CUBE],
-];
-
-foreach ($patterns AS $pattern => $options) {
-    foreach ($controllers AS $c => $item) {
-        $controllers[$c]['label'] = $item['name'];
-        if (preg_match("/$pattern/", $item['name'])) {
-            $favourites[$c] = $item;
-            $favourites[$c]['head'] = $item['name'];
-            $favourites[$c]['label'] = 'Controller';
-            $favourites[$c]['color'] = $options['color'];
-            $favourites[$c]['icon'] = isset($options['icon']) ? $options['icon'] : null;
-            unset($controllers[$c]);
-        }
-    }
-}
 ?>
 
-<?= $this->render(
-    '@vendor/dmstr/yii2-adminlte-asset/example-views/phundament/app/default/_controllers.php',
-    [
-        'controllers' => $controllers,
-        'favourites' => $favourites,
-        'modelNamespace' => 'app\models\\',
-    ]
-) ?>
+<div class="row">
+    <div class="col-sm-4">
+        <?= \insolita\wgadminlte\SmallBox::widget(
+            [
+                'head' => 'Less',
+                'type' => SmallBox::TYPE_LBLUE,
+                'icon' => 'fa fa-'.FA::_PENCIL_SQUARE,
+                'footer' => 'Themes',
+                'footer_link' => ['/prototype/less'],
+            ]) ?>
+    </div>
+    <div class="col-sm-4">
+        <?= \insolita\wgadminlte\SmallBox::widget(
+            [
+                'head' => 'Twig',
+                'type' => SmallBox::TYPE_ORANGE,
+                'icon' => 'fa fa-'.FA::_STICKY_NOTE,
+                'footer' => 'Layouts',
+                'footer_link' => ['/prototype/twig'],
+            ]) ?>
+    </div>
+    <div class="col-sm-4">
+        <?= \insolita\wgadminlte\SmallBox::widget(
+            [
+                'head' => 'HTML',
+                'type' => SmallBox::TYPE_MAR,
+                'icon' => 'fa fa-'.FA::_STICKY_NOTE_O,
+                'footer' => 'Snippets',
+                'footer_link' => ['/prototype/html'],
+            ]) ?>
+    </div>
+</div>
 
+<hr/>
+
+<p>
+    This module provides routes for rendering dynamic views. Content can be varied by request parameter <code>id</code>.
+</p>
+
+<ul>
+    <li><?= Html::a('/prototype/render/twig', ['/prototype/render/twig']) ?></li>
+    <li><?= Html::a('/prototype/render/html', ['/prototype/render/html']) ?></li>
+</ul>
+
+<hr/>
+
+<?= Html::a('Test page', ['test'], ['class' => 'btn btn-default']) ?>
+
+<?= Html::a('Test page with id parameter', ['test', 'id' => '__ID__'], ['class' => 'btn btn-default']) ?>
