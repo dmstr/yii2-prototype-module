33a6f6811833eb0c327067c8bf41986f68e726f3
Tobias Munk <schmunk@usrbin.de>
2017-04-03 15:58:07 +0200

    updated migrations to support postgres

--- src/migrations/m161118_101347_alter_charset_to_utf8.php
+++ src/migrations/m161118_101347_alter_charset_to_utf8.php
@@ -6,12 +6,20 @@ class m161118_101347_alter_charset_to_utf8 extends Migration
 {
     public function up()
     {
-        Yii::$app->db->createCommand("ALTER TABLE {{%html}} CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci ;")
-            ->execute();
-        Yii::$app->db->createCommand("ALTER TABLE {{%less}} CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci ;")
-            ->execute();
-        Yii::$app->db->createCommand("ALTER TABLE {{%twig}} CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci ;")
-            ->execute();
+        $dbName = Yii::$app->db->getDriverName();
+        switch ($dbName) {
+            case 'mysql':
+                Yii::$app->db->createCommand("ALTER TABLE {{%html}} CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci ;")
+                    ->execute();
+                Yii::$app->db->createCommand("ALTER TABLE {{%less}} CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci ;")
+                    ->execute();
+                Yii::$app->db->createCommand("ALTER TABLE {{%twig}} CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci ;")
+                    ->execute();
+                break;
+            default:
+                Yii::info('Character set conversion not supported on your current DBMS');
+                break;
+        }
     }
 
     public function down()
